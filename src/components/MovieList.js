882a48408d8f51353ac799dfbd23cee9cee1ff28
bvrevanth <revanth47.2017@gmail.com>
2023-11-17 17:59:00 +0530

    secondary container with movie list & usePopular movies

---
+++ src/components/MovieList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import MovieCard from "./MovieCard";
+
+const MovieList = ({ title, movies }) => {
+  console.log(movies);
+  return (
+    <div className=" px-6">
+      <h1 className="text-2xl font-normal py-4 text-white">{title}</h1>
+      <div className="flex overflow-x-scroll overflow">
+        <div className="flex space-x-4">
+          {movies?.map((movie) => (
+            <MovieCard key={movie.id} posterPath={movie.poster_path} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,3 +12,5 @@ export const API_OPTIONS = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZTI2ZDBlMzBlMWI5ZDdjNDg5NzRlZmQwODU1OTk0OSIsInN1YiI6IjY1NTViYTM0ZWE4NGM3MTA5MzAwZTNiMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oXB6943uOrcgsSMnnINfBzMeiBBLu-GGmCmnQ_qAG8s",
   },
 };
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
